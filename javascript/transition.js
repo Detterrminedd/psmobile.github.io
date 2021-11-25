9cc41ae709591dbc040478cfd2ccd63e3527bc87
Detterrminedd <detterminedd@gmail.com>
2021-11-25 08:19:28 +0530

    neww

--- javascript/transition.js
+++ javascript/transition.js
@@ -1,31 +1,3 @@
-// $(function(){  // $(document).ready shorthand
-//   $('.monster').fadeout('slow');
-// });
-
-// $(document).ready(function() {
-    
-//     /* Every time the window is scrolled ... */
-//     $(window).scroll( function(){
-    
-//         /* Check the location of each desired element */
-//         $('.firsttext').each( function(i){
-            
-//             var bottom_of_object = $(this).position().top + $(this).outerHeight();
-//             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-//             /* If the object is completely visible in the window, fade it it */
-//             if( bottom_of_window > bottom_of_object ){
-                
-//                 $(this).animate({'opacity':'0'},1500);
-                    
-//             }
-            
-//         }); 
-    
-//     });
-    
-// });
-
 $(window).scroll(function(){
   $(".opacity1").css("opacity", 1 - $(window).scrollTop() / 250);
 });
@@ -49,7 +21,10 @@ $(window).scroll(function(){
 $(window).scroll(function(){
   $(".opacity17").css("opacity", 16 - $(window).scrollTop() / 250);
 });
+// title
+let title = document.querySelector(".title");
 
-
-
-console.log(window, "shaber");
+window.addEventListener("scroll", () => {
+  var top = window.pageYOffset / 30;
+  title.style.transform = "rotate3d(0,1,0, " + top + "deg)";
+});
